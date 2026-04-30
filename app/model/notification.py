dede2d08cadd3d3440fee584cf44e214abf5d338
sebastian velasquez osorio <svelasquez236@soyudemedellin.edu.co>
2026-04-30 18:56:58 -0500

    Solucion bugs y complementacion punto 1 y 2

--- app/model/notification.py
+++ app/model/notification.py
@@ -1,4 +1,29 @@
-from abc import ABC, abstractmethod
+import os
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Protocol, runtime_checkable
+from app.services.util import generate_unique_id
+from abc import abstractmethod,ABC
+
+class NotificationError(Exception):
+    ...
+
+class ChannelUnavailable(NotificationError):
+    is_available = False
+
+class DeliveryError(NotificationError):
+    ...
+
+@runtime_checkable
+class NotificationChanel(ABC):
+    def send(self, message: str) -> None:
+        ...
+
+    def get_channel_name(self) -> str:
+        ...
+
+    def is_available(self) -> bool:
+        ...
 
 class NotificationChannel(ABC):
     @abstractmethod
