539b567a6ed7fe51cd02e79b84cb222d23d40024
jholguin887@soyudemedellin.edu.co <jholguin887fsoyudemedellin.edu.co>
2026-04-30 18:41:59 -0500

    class NotificationError

--- app/model/notification.py
+++ app/model/notification.py
@@ -6,3 +6,20 @@ from app.services.util import generate_unique_id
 
 class NotificationError(Exception):
     ...
+
+class ChannelUnavailable(NotificationError):
+    is_available = False
+
+class DeliveryError(NotificationError):
+    ...
+
+@runtime_checkable
+class NotificationChanel:
+    def send(self, message: str) -> None:
+        ...
+
+    def get_channel_name(self) -> str:
+        ...
+
+    def is_available(self) -> bool:
+        ...
